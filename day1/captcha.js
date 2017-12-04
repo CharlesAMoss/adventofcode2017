9324988eb776201fc56a46cd8e546b58d7addaff
CharlesAMoss <charlesamoss@yahoo.com>
2017-12-03 19:59:34 -0800

    day1 part2 complete

--- day1/captcha.js
+++ day1/captcha.js
@@ -15,6 +15,23 @@ module.exports = {
         }
         
         return nums.reduce((a, b) => a + b)
+    },
+    moreProcess: function(str) {
+        const step = parseInt(str.length / 2);
+        const nums = [0];
+        const splits = str.split('');
+        
+        for (i=0; i < splits.length; i++){
+            if (splits[i] === splits[i+step]) {
+                nums.push(parseInt(splits[i]))
+            } else if (splits[i] === splits[i - step]) {
+                nums.push(parseInt(splits[i]))
+            }
+        }
+    
+        
+        return nums.reduce((a, b) => a + b)
+
     }
 
 };
--- test/test-day1.js
+++ test/test-day1.js
@@ -1,10 +1,20 @@
 const expect  = require('chai').expect;
-const process = require('./../day1/captcha').process;
+const captcha = require('./../day1/captcha');
+
 const fs = require('fs');
+
+// part 1
+
+fs.readFile('./day1/input.txt', function (err, data) {
+    if (err)
+        throw err;
+    if (data)
+        console.log('part 1: ' + captcha.process(data.toString('utf8')));
+})
 
 it('Only repeating numbers are summed', function() {
         
-        const testout = process('1122')
+        const testout = captcha.process('1122')
 
         expect(testout).to.equal(3);
         
@@ -12,7 +22,7 @@ it('Only repeating numbers are summed', function() {
 
 it('number matches the next', function() {
     
-    const testout = process('1111')
+    const testout = captcha.process('1111')
 
     expect(testout).to.equal(4);
     
@@ -20,7 +30,7 @@ it('number matches the next', function() {
 
 it('no repeating number matches', function() {
     
-    const testout = process('1234')
+    const testout = captcha.process('1234')
 
     expect(testout).to.equal(0);
     
@@ -28,18 +38,58 @@ it('no repeating number matches', function() {
 
 it('String is circular, check for that', function() {
     
-    const testout = process('91212129')
+    const testout = captcha.process('91212129')
 
     expect(testout).to.equal(9);
     
 });
 
+// part 2
+
 fs.readFile('./day1/input.txt', function (err, data) {
     if (err)
         throw err;
     if (data)
-        console.log(process(data.toString('utf8')));
+        console.log('part 2: ' + captcha.moreProcess(data.toString('utf8')));
 })
 
+it('List contains 4 items, and all four digits match the digit 2 items ahead', function() {
+    
+    const testout = captcha.moreProcess('1212')
 
+    expect(testout).to.equal(6);
+    
+});
+
+it('Every comparison is between a 1 and a 2.', function() {
+    
+    const testout = captcha.moreProcess('1221')
+
+    expect(testout).to.equal(0);
+    
+});
+
+it('Both 2s match each other, but no other digit has a match', function() {
+    
+    const testout = captcha.moreProcess('123425')
+
+    expect(testout).to.equal(4);
+    
+});
+
+it('Matching cases', function() {
+    
+    const testout = captcha.moreProcess('123123')
+
+    expect(testout).to.equal(12);
+    
+});
+
+it('More matching cases', function() {
+    
+    const testout = captcha.moreProcess('12131415')
+
+    expect(testout).to.equal(4);
+    
+});
 
