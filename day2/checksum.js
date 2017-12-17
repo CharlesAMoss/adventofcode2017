e66ffc6c85ebb677b8a972176e93504140d4d56e
CharlesAMoss <charlesamoss@yahoo.com>
2017-12-16 17:22:30 -0800

    day 2 part 2 passing

--- day2/checksum.js
+++ day2/checksum.js
@@ -3,6 +3,15 @@ function difference(arr) {
     return sorted.pop() - sorted.shift()
 }
 
+function divisible(arr) {
+  const [[first, second]] = arr
+    .map(value => arr.map(inner => [value, inner]))
+    .reduce((curr, next) => [...curr, ...next], [])
+    .filter(([first, second]) => first % second == 0 && first != second)
+
+    return first / second
+}
+
 function process(str) {
     return str.split('\n')
         .map(c => c.replace(/\s+/g, " "))
@@ -11,8 +20,18 @@ function process(str) {
         .reduce((a, b) => a + b)
 }
 
+function moreProcess(str) {
+    return str.split('\n')
+        .map(c => c.replace(/\s+/g, " "))
+        .map(c => c.split(' '))
+        .map(divisible)
+        .reduce((a, b) => a + b)
+}
+
 module.exports = {
     process : process,
-    difference : difference
+    difference : difference,
+    moreProcess : moreProcess,
+    divisible : divisible
 }
 
--- test/test-day2.js
+++ test/test-day2.js
@@ -4,7 +4,8 @@ const checksum = require('./../day2/checksum');
 const fs = require('fs');
 
 // part 1
-fs.readFile('./day2/input-ori.txt', function (err, data) {
+
+fs.readFile('./day2/input.txt', function (err, data) {
     if (err)
         throw err;
     if (data)
@@ -21,10 +22,38 @@ it('Find the diffence between largest and smallest values.', function() {
 
 it('Find the diffence between largest and smallest values on each row, then sum the values.', function() {
     
-    const testout = checksum.process('5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8')
+    const testout = checksum.process('5 1 9 5\n7 5 3\n2 4 6 8')
 
     expect(testout).to.equal(18);
-    
 });
 
+// part 2
+
+fs.readFile('./day2/input.txt', function (err, data) {
+    if (err)
+        throw err;
+    if (data)
+        console.log('part 2: \n\r' + checksum.moreProcess(data.toString('utf8')));
+})
+
+it('Find the evenly divisible values and divide them.', function() {
+    
+    const testout = checksum.divisible([3,8,6,5])
+
+    expect(testout).to.equal(2);
+});
+
+it('Find these and evenly divisible values and divide them.', function() {
+    
+    const testout = checksum.divisible([1224, 343, 1039, 126, 1221, 937, 136, 1185, 1194, 1312, 1217, 929, 124, 1394, 1337, 168])
+   
+    expect(testout).to.equal(9);
+});
+
+it('Find the evenly divisible values on each row and divide them, then sum the values.', function() {
+    
+    const testout = checksum.moreProcess('5 9 2 8\n9 4 7 3\n3 8 6 5')
+
+    expect(testout).to.equal(9);
+});
 
